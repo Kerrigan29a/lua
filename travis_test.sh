a51fa179f5b17b8aae106ea368136bb6c65579ae
Kerrigan29a <kerrigan29a@gmail.com>
2015-01-28 22:44:40 +0100

    Fix in gcov related linker flags

--- travis_test.sh
+++ travis_test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ $1 == "osx" ]; then
-    make macosx test "MYCFLAGS=-fprofile-arcs -ftest-coverage -g -fPIC" "MYLDFLAGS=-lprofile_rt"
+    make macosx test "MYCFLAGS=-fprofile-arcs -ftest-coverage -g -fPIC" "MYLDFLAGS=-fprofile-arcs -ftest-coverage"
 elif [ $1 == "linux" ]; then
-    make linux test  "MYCFLAGS=-fprofile-arcs -ftest-coverage -g -fPIC" "MYLDFLAGS=-lprofile_rt"
+    make linux test  "MYCFLAGS=-fprofile-arcs -ftest-coverage -g -fPIC" "MYLDFLAGS=-fprofile-arcs -ftest-coverage"
 fi
