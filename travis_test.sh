f3c67d656e82b83043683f4caa5d07dcc25dd5d1
Kerrigan29a <kerrigan29a@gmail.com>
2015-02-07 18:42:18 +0100

    Added Coverity Scan support with Travis CI integration

--- travis_test.sh
+++ travis_test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+./undeploy.sh
+./deploy.sh
 if [ $1 == "osx" ]; then
     make macosx test "MYCFLAGS=-fprofile-arcs -ftest-coverage -g -fPIC" "MYLDFLAGS=-fprofile-arcs -ftest-coverage"
 elif [ $1 == "linux" ]; then
